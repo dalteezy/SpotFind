da79e65c446e1ca52d3537944f73920d32da39e6
Dalton <dalteezy@yahoo.com>
2014-09-19 12:09:57 -0700

    map ajax call

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,9 +4,14 @@ class SpotsController < ApplicationController
     @new_spot = Spot.new(spot_params) 
   end
 
+  def create
+    Spot.create(lat:params[:lat], lon:params[:lng])
+    render json: "okay"
+  end
+
 private
   def spot_params
-    params.require(:spot).permit(:location)
+    params.require(:spot).permit(:lat, :lon)
   end
 
 end
